9b22f957b8e7b0bdf53aba3feb5fcfe2b01b9e9b
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-26 00:32:38 +0530

    Time: 0 ms (100%), Space: 12.3 MB (87.18%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-
-        count = 0
         temp = num 
+        count = 0
         while num > 0:
             dig = num % 10
             if dig != 0 and temp % dig == 0:
                 count += 1
-            num = num // 10 # num /= 10
+            num = num // 10
         return count
 
 
