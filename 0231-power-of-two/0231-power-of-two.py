06b15a11247e1769977f5ec044e3ce07274c6008
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-27 20:58:57 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -4,18 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        # for x in range(n+1):
-        #     if n == 2 ** x:
-        #         return True
-        # return False
-
-        # using %2==0 logic
-        if n == 1:
-            return True
-        if n == 0:
+        # optimized -
+        if n <= 0:
             return False
-        while n%2 == 0:
-            n = n // 2 # n //= 2
-            if n == 1:
-                return True
-        return False
+        while n!= 0 and n%2 == 0:
+            n //= 2
+        return n==1
