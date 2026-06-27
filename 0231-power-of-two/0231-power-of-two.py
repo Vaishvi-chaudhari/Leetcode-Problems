1bdb0fb22aa6f589d5f07c5d0e4d8975533e4672
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-27 20:54:14 +0530

    Time: 2 ms (18.7%), Space: 12.4 MB (54.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -8,14 +8,19 @@ class Solution(object):
         #     if n == 2 ** x:
         #         return True
         # return False
-        if n == 1:
-            return True
-        if n == 0:
-            return False
 
-        while n%2 == 0:
-            n = n // 2 # n //= 2
-            if n == 1:
-                return True
-        return False
+        # using %2==0 logic
+        # if n == 1:
+        #     return True
+        # if n == 0:
+        #     return False
+        # while n%2 == 0:
+        #     n = n // 2 # n //= 2
+        #     if n == 1:
+        #         return True
+        # return False
 
+        # optimized -
+        while n!= 0 and n%2 == 0:
+            n //= 2
+        return n==1
