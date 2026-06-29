6719c76a4cc4eb732728cec59a1f4ab032e7a2e5
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-29 20:12:27 +0530

    Time: 66 ms (82.46%), Space: 15.3 MB (26.83%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -5,11 +5,10 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        start = 1 
-
         if n <= 2:
             return n
 
+        start = 1 
         for i in range(2, n):
             if nums[i] != nums[start-1]:
                 start += 1
