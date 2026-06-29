1e0a59bf9cd1e95e4665db95a6d6f4784861e5c2
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-30 00:17:08 +0530

    Time: 111 ms (40.63%), Space: 19.5 MB (14.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        buy_price = prices[0]
+        min_price = prices[0]
         profit = 0
 
         for i in range(1, len(prices)):
             # curr_price = prices[i]
-            curr_profit = prices[i] - buy_price
+            curr_profit = prices[i] - min_price
             if curr_profit > profit:
                 profit = curr_profit
-            buy_price = min(prices[i], buy_price)
+            min_price = min(prices[i], min_price)
 
         return profit
