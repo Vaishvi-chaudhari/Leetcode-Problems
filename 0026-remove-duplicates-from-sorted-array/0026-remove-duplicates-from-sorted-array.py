2412a8ca6e942a050d650a4179f1bc3c780065db
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-29 19:40:51 +0530

    Time: 4 ms (41.77%), Space: 13.9 MB (41.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
         start = 0
 
-        for i in range(1, n):
+        for i in range(1, len(nums)):
             if nums[i] != nums[start]:
                 start += 1
                 nums[start] = nums[i]
