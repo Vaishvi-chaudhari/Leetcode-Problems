f0e30d880718faab94e78d925ff4ec0a609c8098
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-30 00:34:27 +0530

    Time: 0 ms (100%), Space: 13.7 MB (61.89%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -5,7 +5,8 @@ class Solution(object):
         :rtype: int
         """
         profit = 0
-        for i in range(len(prices)-1):
+        n = len(prices)
+        for i in range(n-1):
             if prices[i+1] > prices[i]:
                 profit += prices[i+1] - prices[i]
             
