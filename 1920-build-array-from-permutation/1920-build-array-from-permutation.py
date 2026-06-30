768a3129f5af768d6baf80752bdd0647407d005a
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-06-30 22:34:45 +0530

    Time: 0 ms (100%), Space: 12.7 MB (38.74%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
         ans = []
-        for i in range(0, n):
+        for i in range(0, len(nums)):
             ans.append(nums[nums[i]])
         return ans
