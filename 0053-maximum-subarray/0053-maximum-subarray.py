c1c9f0174c9596bc4f96bcae5b4fb1d18f704e4a
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-01 16:32:35 +0530

    Time: 44 ms (85.92%), Space: 21.3 MB (28.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -12,6 +12,7 @@ class Solution(object):
             curr_sum += nums[i]
             if curr_sum > max_sum:
                 max_sum = curr_sum
+
             if curr_sum < 0:
                 curr_sum = 0
         return max_sum
