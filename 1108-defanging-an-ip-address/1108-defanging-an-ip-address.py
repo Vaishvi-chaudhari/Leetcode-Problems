68db9bf39aca45e85516699951169af8d243fb8a
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-03 23:53:29 +0530

    Time: 12 ms (76.94%), Space: 12.4 MB (14.38%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type address: str
         :rtype: str
         """
-        return address.replace('.', '[.]')
+        ans_str = ""
+        for ch in address:
+            if ch == '.':
+                ans_str += '[.]'
+            else:
+                ans_str += ch
+        return ans_str
