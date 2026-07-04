a113059f34b4c21c2b725d7047ef833c16b33c2b
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-04 23:05:01 +0530

    Time: 264 ms (94.83%), Space: 67.4 MB (15.39%) - LeetHub

--- 2883-drop-missing-data/2883-drop-missing-data.py
+++ 2883-drop-missing-data/2883-drop-missing-data.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    students = students.dropna(subset = ['name'])
-    return students
+    return students.dropna(subset = ['name'])
