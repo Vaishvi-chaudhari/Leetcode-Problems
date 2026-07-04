b3de6de5fe09a460231e3c92269b5b51ef3e5acd
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-04 18:58:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (69.72%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,45 +4,45 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # s = (s.strip()).split()
-        # s.reverse()
-        # return " ".join(s)
+        s = (s.strip()).split()
+        s.reverse()
+        return " ".join(s)
 
-        words = []
-        n = len(s)
-        i = 0
+        # words = []
+        # n = len(s)
+        # i = 0
 
-        # extract words 
-        while i < n:
+        # # extract words 
+        # while i < n:
 
-            # skip spaces
-            while i < n and s[i] == ' ':
-                i += 1
+        #     # skip spaces
+        #     while i < n and s[i] == ' ':
+        #         i += 1
 
-            word = ""
+        #     word = ""
 
-            while i < n and s[i] != ' ':
-                word += s[i]
-                i += 1
+        #     while i < n and s[i] != ' ':
+        #         word += s[i]
+        #         i += 1
 
-            if word != "":
-                words.append(word)
+        #     if word != "":
+        #         words.append(word)
 
-        # Reverse
-        left = 0
-        right = len(words) - 1
+        # # Reverse
+        # left = 0
+        # right = len(words) - 1
 
-        while left < right:
-            words[left], words[right] = words[right], words[left]
-            left += 1
-            right -= 1
+        # while left < right:
+        #     words[left], words[right] = words[right], words[left]
+        #     left += 1
+        #     right -= 1
 
-        # final ans
-        ans = ""
-        for i in range(len(words)):
-            ans += words[i]
+        # # final ans
+        # ans = ""
+        # for i in range(len(words)):
+        #     ans += words[i]
 
-            if i != len(words) - 1:
-                ans += " "
+        #     if i != len(words) - 1:
+        #         ans += " "
 
-        return ans
+        # return ans
