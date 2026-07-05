c1a5655d938ca775beae4de56e1927c11fee6e81
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-05 20:27:23 +0530

    Time: 19 ms (79.34%), Space: 13.1 MB (66.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,13 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        max_len = 0
-        for i in range(len(s)):
-            temp = ""
-            for j in range(i, len(s)):
-                if s[j] in temp:
-                    break
-                temp += s[j]
-                if len(temp) > max_len:
-                    max_len = len(temp)
-        return max_len
+        last = [-1] * 128
+        left = 0
+        ans = 0
+
+        for right in range(len(s)):
+            x = ord(s[right])
+            if last[x] >= left:
+                left = last[x] + 1
+            last[x] = right
+            ans = max(ans, right - left + 1)
+            
+        return ans
+
+        # max_len = 0
+        # for i in range(len(s)):
+        #     temp = ""
+        #     for j in range(i, len(s)):
+        #         if s[j] in temp:
+        #             break
+        #         temp += s[j]
+        #         if len(temp) > max_len:
+        #             max_len = len(temp)
+        # return max_len
