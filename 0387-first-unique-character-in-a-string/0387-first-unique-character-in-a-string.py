def63287a34cef160c371f35c671fda921f29019
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-05 19:34:01 +0530

    Time: 133 ms (21.4%), Space: 15.8 MB (34.64%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
         freq = [0] * 26
         for ch in s:
             freq[ord(ch) - ord('a')] += 1
@@ -12,17 +11,3 @@ class Solution(object):
             if freq[ord(s[i]) - ord('a')] == 1:
                 return i
         return -1
-
-        # for i in range(len(s)):
-        #     count = 0
-        #     for j in range(len(s)):
-        #         if s[i] == s[j]:
-        #             count += 1
-        #     if count == 1:
-        #         return i
-        # return -1
-
-        # for i in range(len(s)):
-        #     if s.count(s[i]) == 1:
-        #         return i
-        # return -1
