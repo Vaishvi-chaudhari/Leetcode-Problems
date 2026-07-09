96c4a2b59638114a6a691672a66a9fa8bf9f49ff
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-09 16:18:02 +0530

    Time: 8 ms (43.23%), Space: 13.3 MB (56.66%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -5,11 +5,15 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        total_sum = sum(nums)
-        left_sum = 0
+        total_sum = 0
 
         for i in range(n):
+            total_sum += nums[i]
+
+        left_sum = 0
+        for i in range(n):
             right_sum = total_sum - left_sum - nums[i]
+            
             if left_sum == right_sum:
                 return i
             left_sum += nums[i]
