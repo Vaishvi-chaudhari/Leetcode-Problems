29e6a4a84befe27ba7b686b52860f305ac2fb72d
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-09 16:28:33 +0530

    Time: 19 ms (49.82%), Space: 12.6 MB (77.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,4 @@
 class Solution(object):
-    def isAlphaNumeric(self, s):
-        x = ord(s)
-        if 97<=x<=122 or 65<=x<=90 or 48<=x<=57:
-            return True
-        return False
-
     def isPalindrome(self, s):
         """
         :type s: str
@@ -15,14 +9,14 @@ class Solution(object):
         r = len(s) - 1
 
         while l < r:
-            if not self.isAlphaNumeric(s[l]):
-                l+=1
-            elif not self.isAlphaNumeric(s[r]):
-                r-=1
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
             elif s[l] == s[r]:
-                l+=1
-                r-=1
-            
+                l += 1
+                r -= 1
             else:
                 return False
+
         return True
