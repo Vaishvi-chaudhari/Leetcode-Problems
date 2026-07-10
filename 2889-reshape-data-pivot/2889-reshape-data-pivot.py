14eaf236a4b6b889c7d625dfc93d0162135c6e3d
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-10 23:43:37 +0530

    Time: 287 ms (89.93%), Space: 66.9 MB (99.86%) - LeetHub

--- 2889-reshape-data-pivot/2889-reshape-data-pivot.py
+++ 2889-reshape-data-pivot/2889-reshape-data-pivot.py
@@ -1,8 +1,4 @@
 import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-        return weather.pivot(
-        index="month",
-        columns="city",
-        values="temperature"
-    )
+        return weather.pivot(index="month",columns="city",values="temperature")
