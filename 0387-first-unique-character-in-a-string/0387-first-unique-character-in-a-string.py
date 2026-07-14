b8c4e10be7231d7d3f8c076a5f90e2f902fa88a9
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-14 22:31:12 +0530

    Time: 91 ms (80.68%), Space: 16 MB (15.72%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -4,10 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        freq = [0] * 26
+        # for i in range(len(s)):
+        #     count = 0
+        #     for j in range(len(s)):
+        #         if s[i] == s[j]:
+        #             count += 1
+        #     if count == 1:
+        #         return i
+        # return -1
+
+        # Optimal : Using Freq of each character -
+        n = len(s)
+        freq = {}
+
         for ch in s:
-            freq[ord(ch) - ord('a')] += 1
-        for i in range(len(s)):
-            if freq[ord(s[i]) - ord('a')] == 1:
+            if ch not in freq:
+                freq[ch] = 1
+            else:
+                freq[ch] += 1
+
+        for i in range(n):
+            if freq[s[i]] == 1:
                 return i
         return -1
