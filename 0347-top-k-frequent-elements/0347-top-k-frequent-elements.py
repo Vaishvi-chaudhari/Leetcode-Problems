fd01e0165968f2ae914e66fb9872c41036b0f683
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-15 18:34:21 +0530

    Time: 15 ms (32.86%), Space: 14.3 MB (56.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,12 +5,27 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # 01 Using : Sorting By Freq
+        # freq = Counter(nums)
+        # sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+        # ans = []
+        # for num, count in sorted_freq:
+        #     ans.append(num)
+        #     if len(ans) == k:
+        #         break
+        # return ans
+
+        # 02 : Using Min Heap / Priority Queue
+        # Priority Queue (Min Heap) : Elements leave based on priority, not insertion order.
         freq = Counter(nums)
-        sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+        heap = []
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
 
         ans = []
-        for num, count in sorted_freq:
-            ans.append(num)
-            if len(ans) == k:
-                break
+        while heap:
+            ans.append(heapq.heappop(heap)[1])
         return ans
