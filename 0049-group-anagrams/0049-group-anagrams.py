a4eac869f1ef32bbcc7b60e9ab0d6e3ba02a1de5
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-15 17:02:38 +0530

    Time: 22 ms (53.71%), Space: 16 MB (67.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,19 @@
 class Solution(object):
+    def sortString(self, s):
+        s1 = list(s)
+        s1.sort()
+        return "". join(s1)
+
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        groups = defaultdict(list)
-        for word in strs:
-            key = ''.join(sorted(word))
-            groups[key].append(word)
-
-        return list(groups.values())
+        dict1 = {}
+        for s in strs:
+            key = self.sortString(s)
+            if key in dict1:
+                dict1[key].append(s)
+            else:
+                dict1[key] = [s]
+        return list(dict1.values())
