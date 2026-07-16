e5659c5cec68d7a6efb334e155801748ba0ea467
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-16 17:25:33 +0530

    Time: 3 ms (73.15%), Space: 12.4 MB (55.87%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
@@ -5,13 +5,19 @@ class Solution(object):
         :type m: int
         :rtype: int
         """
-        num1 = 0
-        num2 = 0
+        # num1 = 0
+        # num2 = 0
 
-        for i in range(1, n + 1):
-            if i % m == 0:
-                num2 += i
-            else:
-                num1 += i
-                
-        return num1 - num2
+        # for i in range(1, n + 1):
+        #     if i % m == 0:
+        #         num2 += i
+        #     else:
+        #         num1 += i
+
+        # return num1 - num2
+
+        total = n * (n + 1) // 2
+        k = n // m
+        divisible_sum = m * (k * (k + 1) // 2)
+
+        return total - 2 * divisible_sum
