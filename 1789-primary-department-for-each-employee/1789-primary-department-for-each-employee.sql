946eb46e99dd0aaa09dd3f3bed50a11ce569539f
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-17 21:49:45 +0530

    Time: 3789 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+OR employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+    );
+
+-- USING UNION :
+-- SELECT employee_id, department_id
+-- FROM Employee
+-- WHERE primary_flag = 'Y'
+-- UNION
+-- SELECT employee_id, department_id
+-- FROM Employee
+-- GROUP BY employee_id, department_id
+-- HAVING COUNT(*) = 1;
