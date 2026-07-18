8a111762001e24232ac90b6bc24e2328590e4a96
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-07-18 14:29:55 +0530

    Time: 41 ms (30.88%), Space: 12.5 MB (86.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
+        count = [0] * 26
+        for ch in magazine:
+            count[ord(ch) - ord('a')] += 1
+
+        for ch in ransomNote:
+            idx = ord(ch) - ord('a')
+            if count[idx] == 0:
+                return False
+            count[idx] -= 1
+        return True
+
         # magazine = list(magazine)
         # for ch in ransomNote:
         #     if ch in magazine:
@@ -13,11 +24,11 @@ class Solution(object):
         #         return False
         # return True
 
-        freq = {}
-        for ch in magazine:
-            freq[ch] = freq.get(ch, 0) + 1
-        for ch in ransomNote:
-            if freq.get(ch, 0) == 0:
-                return False
-            freq[ch] -= 1
-        return True
+        # freq = {}
+        # for ch in magazine:
+        #     freq[ch] = freq.get(ch, 0) + 1
+        # for ch in ransomNote:
+        #     if freq.get(ch, 0) == 0:
+        #         return False
+        #     freq[ch] -= 1
+        # return True
