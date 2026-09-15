dd03fdf27279874d1832e8048a8f245511f96542
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-09-15 18:51:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (77.08%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,41 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution(object):
-    def rotateRight(self, head, k):
-        """
-        :type head: Optional[ListNode]
-        :type k: int
-        :rtype: Optional[ListNode]
-        """
-        if not head or not head.next or k == 0:
+class Solution:
+    def rotateRight(self, head: ListNode | None, k: int) -> ListNode | None:
+        if head == None or head.next == None:
             return head
 
-        length = 1
-        tail = head
-        while tail.next:
-            tail = tail.next
-            length += 1
+        l = 0
+        last = head
+        while last.next != None:
+            last = last.next
+            l += 1
 
-        k %= length
+        l += 1
+        k = k % l
         if k == 0:
             return head
-        tail.next = head
-        steps = length - k - 1
-        new_tail = head
 
-        for _ in range(steps):
-            new_tail = new_tail.next
+        curr = head
+        for i in range(l - k - 1):
+            curr = curr.next
 
-        new_head = new_tail.next
-        new_tail.next = None
+        last.next = head
+        head = curr.next
+        curr.next = None
 
-        return new_head
+        return head
