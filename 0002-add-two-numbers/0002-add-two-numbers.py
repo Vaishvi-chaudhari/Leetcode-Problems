7ad9a480fdfe28d1b9af3e24da493759f6c8bf46
Vaishvi Chaudhari <vaishvichaudhary11@gmail.com>
2026-09-16 23:31:53 +0530

    Time: 4 ms (44.54%), Space: 19.2 MB (96.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,29 +1,54 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        dummy = ListNode(0)
-        current = dummy
-        carry = 0
+class Solution:
+    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
 
-        while l1 or l2 or carry:
-            total = carry
-            if l1:
-                total += l1.val
-                l1 = l1.next
-            if l2:
-                total += l2.val
-                l2 = l2.next
+        l1_len = 0
+        l2_len = 0
+        curr1 = l1
+        curr2 = l2
+
+        while curr1 != None:
+            curr1 = curr1.next
+            l1_len += 1
+        while curr2 != None:
+            curr2 = curr2.next
+            l2_len += 1
+
+        def addZeros(head, count):
+            curr = head
+            while curr.next != None:
+                curr = curr.next
+
+            for _ in range(count):
+                curr.next = ListNode(0)
+                curr = curr.next
+            return head
+
+        if l1_len < l2_len:
+            l1 = addZeros(l1, l2_len - l1_len)
+        elif l2_len < l1_len:
+            l2 = addZeros(l2, l1_len - l2_len)
+
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        curr1 = l1
+        curr2 = l2
+
+        while curr1 != None:
+            total = curr1.val + curr2.val + carry
             carry = total // 10
-            current.next = ListNode(total % 10)
-            current = current.next
+            digit = total % 10
+            curr.next = ListNode(digit)
+            curr = curr.next
+            curr1 = curr1.next
+            curr2 = curr2.next
+
+        if carry:
+            curr.next = ListNode(carry)
 
         return dummy.next
